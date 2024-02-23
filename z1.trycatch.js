bf9e9b2c239bc6971e10a41f052bd0d3149960d8
shahsuresh <shahsuresh75@gmail.com>
2024-02-23 13:56:34 +0545

    async-await

---
+++ z1.trycatch.js
@@ -0,0 +1,11 @@
+//? try catch => error handling
+
+const x = 5;
+
+try {
+  x = 10;
+} catch (error) {
+  console.log(error.message);
+}
+console.log("Hello 1");
+console.log("Kathmandu");
---
+++ z2.async_await.js
@@ -0,0 +1,25 @@
+// const posts = async () => {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+// posts();
+
+//?========================================
+
+const getData = async () => {
+  try {
+    const response = await fetch("https://api.escuelajs.co/api/v1/products");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
